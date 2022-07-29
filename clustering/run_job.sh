034407c24092d73ba5b77beee0e7ec21eedbf1dc
James Tan <james_tan97@outlook.com>
2022-07-29 00:13:31 -0400

    Add clustering scripts

--- clustering/run_job.sh
+++ clustering/run_job.sh
@@ -8,7 +8,7 @@ gcloud beta dataproc clusters create ${CLUSTER_NAME} \
     --image-version=2.0 \
     --metadata "PIP_PACKAGES=google-cloud-storage ${PIP_PACKAGES}" \
     --worker-machine-type n1-standard-8 \
-    --num-workers 2 \
+    --num-workers 6 \
     --initialization-actions gs://dataproc-initialization-actions/python/pip-install.sh \
     --enable-component-gateway \
     --project="${PROJECT_NAME}" \
@@ -19,4 +19,4 @@ gcloud dataproc jobs submit pyspark --cluster ${CLUSTER_NAME}\
     --properties=spark.jars.packages=graphframes:graphframes:0.8.2-spark3.1-s_2.12\
     --driver-log-levels root=FATAL \
     --project="${PROJECT_NAME}" \
-    dummy-clustering.py
+    actual-data-clustering-poc.py
